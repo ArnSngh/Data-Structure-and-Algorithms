dffa5aaef4d5c60982fb980c5ca6400459dbc10f
Aryan Singh <168927699+ArnSngh@users.noreply.github.com>
2026-04-06 10:24:16 +0530

    Time: 3 ms (20.46%), Space: 12.6 MB (5.06%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,32 +1,57 @@
+
 class Solution {
 public:
+
     string addBinary(string a, string b) {
-        int i = a.size() - 1;
-        int j = b.size() - 1;
+        
+        
+        if(b.size() > a.size()) swap(a,b);
+        
+        
+        while(b.size() < a.size()) b = "0" + b;
+
         int carry = 0;
-        
-        string result = "";
-        
-        while (i >= 0 || j >= 0 || carry) {
-            int sum = carry;
-            
-            if (i >= 0) {
-                sum += a[i] - '0';
-                i--;
-            }
-            
-            if (j >= 0) {
-                sum += b[j] - '0';
-                j--;
-            }
-            
-            result += (sum % 2) + '0';
-            
-            carry = sum / 2;
+
+        string res = "";
+
+        for(int i = b.size()-1; i >= 0 ; --i)
+        {
+             
+             if(b[i] == '1' && a[i]=='1')
+             {
+
+                if(carry == 0) res = "0" + res;
+                
+                else res = "1" + res;
+                    
+                carry = 1;
+             }
+
+             else if(b[i] =='0' && a[i] =='0')
+             {
+
+                if(carry == 0) res = "0" + res;
+                 
+                else
+                {
+                    res = "1" + res;
+                    carry = 0;
+                }
+             }
+
+             else if((b[i]=='0' && a[i]=='1') || (b[i]=='1' && a[i] == '0'))
+             {
+                 
+                if(carry == 0) res = "1" + res;
+                 
+                else res = "0" + res;
+                 
+             }
+             
         }
         
-        reverse(result.begin(), result.end());
+        if(carry == 1) res = "1" + res;
         
-        return result;
+        return res;
     }
 };
