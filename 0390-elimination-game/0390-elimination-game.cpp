d8f3e26ac60c06744778d736f9fec1cf8cac4589
Aryan Singh <168927699+ArnSngh@users.noreply.github.com>
2026-04-22 23:38:59 +0530

    Time: 0 ms (100%), Space: 9.7 MB (45.27%) - LeetHub

---
+++ 0390-elimination-game/0390-elimination-game.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool changeBothPtr(int& left, int& right, int& step) {
+        return !((right - left) % step);
+    }
+
+    int lastRemaining(int n) {
+        int left = 1, right = n, step = 2, pos = 0;
+
+        while (left < right) {
+            if (changeBothPtr(left, right, step)) {
+                left+= (step / 2);
+                right-= (step / 2);
+            }
+            else {
+                if (pos) right-= (step / 2);
+                else left+= (step / 2);
+            }
+            pos = 1 - pos;
+            step*= 2;
+        }
+        return left;
+    }
+};
