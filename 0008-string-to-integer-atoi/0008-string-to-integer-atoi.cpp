6ea675948636fe536e48c0c9f985cbbc0e2db0c6
Aryan Singh <168927699+ArnSngh@users.noreply.github.com>
2026-05-25 01:34:59 +0530

    Time: 4 ms (5.68%), Space: 8.9 MB (97.89%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,33 +1,69 @@
 class Solution {
 public:
+
+
+
+// done by me hehe XD
+
+
+
+
     int myAtoi(string s) 
     {
-        int i=0;
-        int sign=1;
-        long ans=0;
-        while(i<s.length() && s[i]==' ')
-            i++;
-        if(s[i]=='-')
-        {
-            sign=-1;
-            i++;
+      int max = INT_MAX/10;
+      int min = INT_MIN/10;
+      int i = 0;
+      int l = s.length();
+      int digit = 0;
+      long long value = 0;
+      bool positive = true;
+      
+
+      //removing whitespace
+      while(i < l && isspace(s[i])){
+        i++;
+      }
+
+      //empty string
+      if(i == l){
+        return 0;
+      }
+
+      //checking sign
+      if(s[i] == '-'){
+        positive = false;
+        i++;
+      }
+      else if(s[i] == '+'){
+        i++;
+      }
+
+      //cases like -aeffdsa
+      if(i == l || !isdigit(s[i])){
+        return 0;
+      }
+
+      while(i < l && isdigit(s[i])){
+        
+        digit = s[i] - '0';
+
+        //overflow handling
+        if(value > INT_MAX/10 || 
+          (value == INT_MAX/10 && digit > 7)){
+            
+            return positive ? INT_MAX : INT_MIN;
         }
-        else if(s[i]=='+')
-            i++;
-        while(i<s.length())
-        {
-            if(s[i]>='0' && s[i]<='9')
-            {
-                ans=ans*10+(s[i]-'0');
-                if(ans>INT_MAX && sign==-1)
-                    return INT_MIN;
-                else if(ans>INT_MAX && sign==1)
-                    return INT_MAX;
-                i++;
-            }
-            else
-                return ans*sign;
-        }
-        return (ans*sign);
+
+        value = value * 10 + digit;
+        i++;
+      }
+
+      if(!positive){
+        value = value * (-1);
+      }
+
+      return value;
+
     }
+      
 };
