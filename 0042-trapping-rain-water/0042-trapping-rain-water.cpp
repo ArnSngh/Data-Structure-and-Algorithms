a8b7e2fd4bb4c54b5788dc778dec0eb4b865c79f
Aryan Singh <168927699+ArnSngh@users.noreply.github.com>
2026-05-27 18:44:14 +0530

    Time: 0 ms (100%), Space: 27.4 MB (15.98%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,30 +1,23 @@
-class Solution
-{
+class Solution {
 public:
-    int trap(vector<int> &height)
-    {
-        int n = height.size(); // Number of elements in the input vector
-        int water = 0;     // Variable to store the total trapped water
+    int trap(vector<int>& height) {
+        int ans = 0;
+        int n = height.size();
+        vector<int>lmax(n);
+        vector<int>rmax(n);
 
-        // Arrays to store maximum height to the left and right of each index
-        vector<int> l_max(n), r_max(n);
-
-        // Calculate the maximum height to the left of each index
-        l_max[0] = height[0]; // First element's maximum height to the left is itself
-        for (int i = 1; i < n; i++)
-            l_max[i] = max(height[i], l_max[i - 1]);
-
-        // Calculate the maximum height to the right of each index
-        r_max[n - 1] = height[n - 1]; // Last element's maximum height to the right is itself
-        for (int i = n - 2; i >= 0; i--)
-            r_max[i] = max(height[i], r_max[i + 1]);
-
-        // Iterate through each index (excluding the first and last)
-        for (int i = 1; i < n - 1; i++)
-        {
-            // Calculate the maximum water current height can hold above itself
-            water += min(r_max[i], l_max[i]) - height[i];
+        lmax[0]=height[0];
+        rmax[n-1] = height[n-1];
+        for(int i = 1; i<n; i++){
+            lmax[i] = max(height[i], lmax[i-1]);
+            
         }
-        return water; // Return the total trapped water
+        for(int i = n-2; i>=0; i--){
+            rmax[i] = max(height[i], rmax[i+1]);
+        }
+        for(int i = 0; i<n; i++){
+            ans = ans + min(lmax[i], rmax[i]) - height[i];
+        }
+        return ans;
     }
 };
