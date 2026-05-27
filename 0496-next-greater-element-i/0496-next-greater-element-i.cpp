26ede3a0a04d958af832f284f66a2a8a0a1936b2
Aryan Singh <168927699+ArnSngh@users.noreply.github.com>
2026-05-27 12:17:51 +0530

    Time: 43 ms (5.62%), Space: 24.9 MB (13.44%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,30 +1,36 @@
-
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        
+
         vector<int> ans;
-        bool found = false;
-        int greater;
 
-        for(int i = 0; i < nums1.size(); i++) {
+        for (int i = 0; i < nums1.size(); i++) {
 
-            greater = -1;
-            found = false;
+            stack<int> st;
+            bool found = false;
 
-            for(int j = 0; j < nums2.size(); j++) {
+            // traverse nums2 from right to left
+            for (int j = nums2.size() - 1; j >= 0; j--) {
 
-                if(nums1[i] == nums2[j]) {
-                    found = true;
+                while (!st.empty() && st.top() <= nums2[j]) {
+                    st.pop();
                 }
 
-                if(found == true && nums2[j] > nums1[i]) {
-                    greater = nums2[j];
+                // when nums1[i] found in nums2
+                if (nums2[j] == nums1[i]) {
+                    found = true;
+
+                    if (st.empty()) {
+                        ans.push_back(-1);
+                    } else {
+                        ans.push_back(st.top());
+                    }
+
                     break;
                 }
-            }
 
-            ans.push_back(greater);
+                st.push(nums2[j]);
+            }
         }
 
         return ans;
