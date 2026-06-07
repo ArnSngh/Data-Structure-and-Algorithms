865997c8123b4433ec7231e23d36c9cdbed2e4b0
Aryan Singh <aryansingh86855@gmail.com>
2026-06-08 04:10:21 +0530

    Time: 0 ms (100%), Space: 9.5 MB (28.62%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    int solve(int m, int n, int row, int col, vector<vector<int>> &dp) {
+
+        if(row == m-1 && col == n-1){
+            return 1;
+        }
+
+        if(row >= m || col >= n){
+            return 0;
+        }
+
+        if(dp[row][col] != -1){
+            return dp[row][col];
+        }
+
+        int down = solve(m, n, row + 1, col, dp);
+        int right = solve(m, n, row, col + 1, dp);
+
+        return dp[row][col] = down + right;
+    }
+
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(m, n, 0, 0, dp);
+    }
+};
