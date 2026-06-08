aae00086951af789673f548cebb965801cce9ee4
Aryan Singh <aryansingh86855@gmail.com>
2026-06-09 03:50:59 +0530

    Time: 0 ms (100%), Space: 13.3 MB (40.94%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,43 +1,35 @@
 class Solution {
-
-    int solve(int row, int col,vector<vector<int>>& triangle, vector<vector<int>>& dp ){
-        if(row == 0 && col == 0){
-            return triangle[0][0];
-        }
-        if(row<0|| col<0 || col>row){
-            return 1e9;
-        }
-        if(dp[row][col] !=-1){
-            return dp[row][col];
-        }
-        int a = INT_MAX;
-        int b = INT_MAX;
-        if(row>0){
-            a =triangle[row][col]+ solve(row-1, col, triangle, dp);
-        }
-        if(col>0){
-            b = triangle[row][col]+ solve(row-1, col-1, triangle, dp);
-        }
-        return dp[row][col] = min(a,b);
-
-        
-    }
-
-
-
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int row = triangle.size();
-        int col = triangle[row-1].size();
-        vector<vector<int>> dp(row, vector<int>(col, -1));
+
+        int n = triangle.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        dp[0][0] = triangle[0][0];
+
+        for(int r = 1; r < n; r++) {
+            for(int c = 0; c <= r; c++) {
+
+                int up = 1e9;
+                int diagonal = 1e9;
+
+                if(c < r)
+                    up = triangle[r][c] + dp[r-1][c];
+
+                if(c > 0)
+                    diagonal = triangle[r][c] + dp[r-1][c-1];
+
+                dp[r][c] = min(up, diagonal);
+            }
+        }
+
         int ans = 1e9;
 
-        for(int col = 0; col < triangle[row - 1].size(); col++) {
-            ans = min(ans, solve(row - 1, col, triangle, dp));
+        for(int c = 0; c < n; c++) {
+            ans = min(ans, dp[n-1][c]);
         }
 
         return ans;
-
-        
     }
 };
