cf2017b1c204c5bb5939905d104f3d29038012a5
Aryan Singh <aryansingh86855@gmail.com>
2026-06-09 02:35:16 +0530

    Time: 0 ms (100%), Space: 15.8 MB (75.1%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,35 +1,38 @@
 class Solution {
-
-    int solve( int row, int col,vector<vector<int>>& grid) {
-        vector<vector<int>> dp(row, vector<int>(col, -1));
-        
-        for(int r = 0; r<row; r++){
-            for(int c = 0; c<col; c++){
-                int left = INT_MAX;
-                int up = INT_MAX;
-                if(r == 0 && c == 0){
-                    dp[0][0] = grid[0][0];
-                    continue;
-                }
-                if(c>0){
-                    left = grid[r][c] + dp[r][c-1];
-                }
-                if(r>0){
-                    up = grid[r][c] + dp[r-1][c];
-                }
-                dp[r][c] = min(left, up);
-            }
-        }
-        return dp[row-1][col-1];
-    }
-
-
-
 public:
     int minPathSum(vector<vector<int>>& grid) {
+
         int row = grid.size();
         int col = grid[0].size();
-        return solve(row, col, grid );
-        
+
+        vector<int> prev(col, 0);
+
+        for(int r = 0; r < row; r++) {
+
+            vector<int> curr(col, 0);
+
+            for(int c = 0; c < col; c++) {
+
+                if(r == 0 && c == 0) {
+                    curr[c] = grid[0][0];
+                    continue;
+                }
+
+                int left = 1e9;
+                int up = 1e9;
+
+                if(c > 0)
+                    left = grid[r][c] + curr[c-1];
+
+                if(r > 0)
+                    up = grid[r][c] + prev[c];
+
+                curr[c] = min(left, up);
+            }
+
+            prev = curr;
+        }
+
+        return prev[col-1];
     }
 };
