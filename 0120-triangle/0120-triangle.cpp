f6fad9719fb3a7b523a17bba332fdf08b60a02a0
Aryan Singh <aryansingh86855@gmail.com>
2026-06-09 03:45:44 +0530

    Time: 0 ms (100%), Space: 13.2 MB (53.8%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,13 +1,43 @@
 class Solution {
+
+    int solve(int row, int col,vector<vector<int>>& triangle, vector<vector<int>>& dp ){
+        if(row == 0 && col == 0){
+            return triangle[0][0];
+        }
+        if(row<0|| col<0 || col>row){
+            return 1e9;
+        }
+        if(dp[row][col] !=-1){
+            return dp[row][col];
+        }
+        int a = INT_MAX;
+        int b = INT_MAX;
+        if(row>0){
+            a =triangle[row][col]+ solve(row-1, col, triangle, dp);
+        }
+        if(col>0){
+            b = triangle[row][col]+ solve(row-1, col-1, triangle, dp);
+        }
+        return dp[row][col] = min(a,b);
+
+        
+    }
+
+
+
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        for (int row = n - 2; row >= 0; row--) {
-            for (int col = 0; col < triangle[row].size(); col++) {
-                triangle[row][col] += min(triangle[row + 1][col], 
-                                          triangle[row + 1][col + 1]);
-            }
+        int row = triangle.size();
+        int col = triangle[row-1].size();
+        vector<vector<int>> dp(row, vector<int>(col, -1));
+        int ans = 1e9;
+
+        for(int col = 0; col < triangle[row - 1].size(); col++) {
+            ans = min(ans, solve(row - 1, col, triangle, dp));
         }
-        return triangle[0][0];
+
+        return ans;
+
+        
     }
 };
