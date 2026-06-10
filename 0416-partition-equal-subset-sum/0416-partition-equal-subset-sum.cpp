4a1ce0b1d2819375b607e5e36e39aa28040a3389
Aryan Singh <aryansingh86855@gmail.com>
2026-06-11 04:25:17 +0530

    Time: 211 ms (9.21%), Space: 262.2 MB (5.1%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,21 +1,36 @@
 class Solution {
+    int solve(vector<int> &nums, int target, int index, vector<vector<int>> & dp){
+         if(target == 0){
+            return true;
+        }
+        if(index == nums.size()){
+            return false;
+        }
+        if(dp[index][target] != -1){
+            return dp[index][target]; // it will never return -1 , only return 0 and 1
+        }
+       
+        bool pick = false;
+        if(nums[index] <= target){
+            pick = solve(nums, target - nums[index], index +1, dp);
+        }
+        bool unpick = solve(nums, target, index +1, dp);
+
+        return dp[index][target] =  pick || unpick;
+
+    }
 public:
     bool canPartition(vector<int>& nums) {
-        int total = accumulate(nums.begin(), nums.end(), 0);
-        if (total % 2 != 0) return false;
-
-        int target = total / 2;
-        vector<bool> dp(target + 1, false);
-        dp[0] = true;
-
-        for (int n : nums) {
-            for (int i = target; i >= n; i--) {
-                if (dp[i]) continue;
-                if (dp[i - n]) dp[i] = true;
-                if (dp[target]) return true;
-            }
+        
+        int sum = 0;
+        for(int i = 0; i<nums.size(); i++){
+            sum = sum + nums[i];
         }
-
-        return false;        
+        vector<vector<int>> dp(nums.size(), vector<int>(sum+1, -1));
+        if(sum%2 != 0){
+            return false;
+        }
+        return solve(nums, sum/2, 0, dp);
+        
     }
 };
