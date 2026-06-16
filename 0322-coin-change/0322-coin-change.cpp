5cf568bb2102d34598e26699049e2e67504e296c
Aryan Singh <aryansingh86855@gmail.com>
2026-06-17 05:28:03 +0530

    Time: 50 ms (24.88%), Space: 49 MB (33.94%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,38 +1,32 @@
 class Solution {
-    int solve(vector<int>& coins, int amount,int ind,  vector<vector<int>> &dp){
-
-        if(amount == 0){
-            return 0;
-        }
-        if(ind ==0 ){
-            if(amount % coins[0] == 0){
-                return amount/coins[0];
-            }
-            else{
-                return 1e9;
-            }
-        }
-        if(dp[ind][amount] != -1){
-            return dp[ind][amount];
-        }
-
-        int take = 1e9;
-        if(coins[ind]<= amount){
-            take = 1+ solve(coins, amount-coins[ind], ind, dp);
-        }
-        int untake = solve(coins, amount, ind-1, dp);
-
-
-        return dp[ind][amount] = min(take , untake);
-    }
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size(), vector<int>(amount+1, -1));
-        int ans = solve(coins, amount, coins.size()-1, dp);
-        if(ans >= 1e9){
+        vector<vector<int>> dp(coins.size(), vector<int>(amount+1, 0));
+        for(int ind = 0; ind<coins.size(); ind++){
+            for(int target = 0; target<= amount; target++){
+                if(ind == 0){
+                    if(target%coins[0] == 0){
+                      dp[ind][target] = target/coins[0];
+                    }
+                    else{
+                     dp[ind][target] = 1e9;
+                 }
+                 continue;
+                }
+
+                int take = 1e9;
+                if(coins[ind] <= target){
+                    take = 1 + dp[ind][target-coins[ind]];
+                }
+                int notake = dp[ind-1][target];
+                dp[ind][target] = min(take, notake);
+            }
+        }
+
+        if(dp[coins.size()-1][amount]>= 1e9){
             return -1;
         }
-        return ans;
+        else return dp[coins.size()-1][amount];
         
     }
 };
