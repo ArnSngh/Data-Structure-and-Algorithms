014024622b0e9b0787541a59da1370d925cf4209
Aryan Singh <aryansingh86855@gmail.com>
2026-06-23 05:15:14 +0530

    Time: 529 ms (5.38%), Space: 277.3 MB (5.06%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,23 +2,35 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        if (n == 1) return 0;
-        vector<int> left_sell(n,0);
-        vector<int> right_sell(n,0);
-        int max_price = prices[0];
-        for(int i = 1; i < n; i++){
-            max_price = min(max_price, prices[i]);
-            left_sell[i] = max(left_sell[i-1], -max_price + prices[i]);
+
+        vector<vector<vector<int>>> dp(
+            n + 1,
+            vector<vector<int>>(3, vector<int>(2, 0))
+        );
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 2; buy >= 0; buy--) {
+                for (int flag = 1; flag >= 0; flag--) {
+
+                    if (buy == 0) {
+                        dp[i][buy][flag] = 0;
+                        continue;
+                    }
+
+                    if (flag == 1) {
+                        dp[i][buy][flag] =
+                            max(-prices[i] + dp[i + 1][buy][0],
+                                dp[i + 1][buy][1]);
+                    } 
+                    else {
+                        dp[i][buy][flag] =
+                            max(prices[i] + dp[i + 1][buy - 1][1],
+                                dp[i + 1][buy][0]);
+                    }
+                }
+            }
         }
-        max_price = prices[n-1];
-        for(int i = n-2; i>=0; i--) {
-            max_price = max(prices[i], max_price);
-            right_sell[i] = max(right_sell[i+1], -prices[i] + max_price);
-        }
-        int max_profit = 0;
-        for(int i = 0; i < n; i++) {
-            max_profit =  max((right_sell[i]+left_sell[i]), max_profit);
-        }
-        return max_profit;
-    } 
+
+        return dp[0][2][1];
+    }
 };
