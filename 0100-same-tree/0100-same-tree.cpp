33d7d01c701b00fdcd57fccdab8cee62dcd56c11
Aryan Singh <aryansingh86855@gmail.com>
2026-06-26 03:18:33 +0530

    Time: 0 ms (100%), Space: 12.9 MB (12.86%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -1,9 +1,17 @@
-
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(!p && !q)    return true;
-        if(!p || !q)    return false;
-            return (p->val == q->val && isSameTree(p->right, q->right) && isSameTree(p->left, q->left));
+        //me define kr deta mereko kha kha false milega kya kya condition pe dikkat hogi fir return krdunga last me left recursive call and right recursive call ka && taaki agr ek bhi false ho to loda false return krde
+        if(p == nullptr && q== nullptr){
+            return true;
+        }
+        // agr dono saath khtm hogye to thik hai but agr alag hue to galat baat hai
+        if(p == nullptr || q == nullptr){
+            return false;
+        }
+        if(p->val!= q->val){
+            return false;
+        }
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
