163bff8201e1b2544169d588632bcdbdb03102cb
Aryan Singh <aryansingh86855@gmail.com>
2026-06-26 04:34:36 +0530

    Time: 0 ms (100%), Space: 15.1 MB (47.61%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,47 +1,40 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        if (!root)
-            return {};
         vector<vector<int>> ans;
-        bool direction = 0;
+        if(root==nullptr){
+            return ans;
+        }
         queue<TreeNode*> q;
         q.push(root);
-        while (!q.empty()) {
-            int n = q.size();
-            vector<int> temp;
-            //process all elements of the same level
-            while (n) {
-                TreeNode* frontNode = q.front();
-                q.pop();
-                temp.push_back(frontNode->val);
-                if (frontNode->left)
-                    q.push(frontNode->left);
-                if (frontNode->right)
-                    q.push(frontNode->right);
-                n--;
+        bool LTR = 1;
+
+        while(!q.empty()) {//each 1 cycle
+        int size = q.size();
+        vector<int> temp(size);
+        for(int i = 0; i<size; i++){ // this loop is for traversing through each element in queue in this current cycle
+            TreeNode* node = q.front(); // lets pickup the first element
+            // now we want to push this element in temp according to the flag wheter at starting or at last...and then push its children in the queue itself
+            q.pop(); // remove that paretn node
+            if(LTR){
+                // we want to fill left to right then we will fill the temp vector from starting that is from i
+                temp[i] = node->val;
             }
-            if (direction == 0 && !temp.empty()) {
-                ans.push_back(temp);
-                direction = 1;
-            } else if (direction == 1 && !temp.empty()) {
-                reverse(temp.begin(), temp.end());
-                ans.push_back(temp);
-                direction = 0;
+            else{
+                temp[size-1-i] = node->val; //agr LTR nhi hai to last element bhrna chaalu krenge...lets say size = 4 them temp[4-1-0] = temp[3] mtlb last element
+            }
+            if(node->left){
+                q.push(node->left);
+            }
+            if(node->right){
+                q.push(node->right);
             }
         }
+        LTR = !LTR; // reverse the direction
+        ans.push_back(temp);        
+        }
         return ans;
+
+        
     }
 };
