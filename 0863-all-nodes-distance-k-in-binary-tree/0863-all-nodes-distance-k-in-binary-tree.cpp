dffe6b659dc541bfbb7f33e86384e8dac13b388c
Aryan Singh <aryansingh86855@gmail.com>
2026-07-06 12:25:08 +0530

    Time: 0 ms (100%), Space: 15.1 MB (90.25%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,77 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        unordered_map<TreeNode*, TreeNode*> parent;
+        queue<TreeNode*> q;
+
+        // Step 1: Build parent map
+        q.push(root);
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            if (node->left) {
+                parent[node->left] = node;
+                q.push(node->left);
+            }
+
+            if (node->right) {
+                parent[node->right] = node;
+                q.push(node->right);
+            }
+        }
+
+        // Step 2: BFS from target
+        unordered_map<TreeNode*, bool> vis;
+        q.push(target);
+        vis[target] = true;
+
+        int dist = 0;
+
+        while (!q.empty()) {
+            int sz = q.size();
+
+            if (dist == k)
+                break;
+
+            while (sz--) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                if (node->left && !vis[node->left]) {
+                    vis[node->left] = true;
+                    q.push(node->left);
+                }
+
+                if (node->right && !vis[node->right]) {
+                    vis[node->right] = true;
+                    q.push(node->right);
+                }
+
+                if (parent.count(node) && !vis[parent[node]]) {
+                    vis[parent[node]] = true;
+                    q.push(parent[node]);
+                }
+            }
+
+            dist++;
+        }
+
+        vector<int> ans;
+        while (!q.empty()) {
+            ans.push_back(q.front()->val);
+            q.pop();
+        }
+
+        return ans;
+    }
+};
