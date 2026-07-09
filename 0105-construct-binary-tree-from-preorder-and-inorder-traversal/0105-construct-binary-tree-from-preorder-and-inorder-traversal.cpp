69b4f9d14e7b62381cecba58b9e73a209fb0531a
Aryan Singh <aryansingh86855@gmail.com>
2026-07-09 17:14:49 +0530

    Time: 7 ms (34.08%), Space: 27.4 MB (68.61%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,34 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    map <int, int> mp;
-    int c = 0;
+    unordered_map<int, int> mp;
+    int preIndex = 0;
 
-    TreeNode* get(vector <int> &a, int s, int f) {
-        if (s > f) return nullptr;
+    TreeNode* build(vector<int>& preorder, int inStart, int inEnd) {
+        if (inStart > inEnd)
+            return NULL;
 
-        TreeNode* v = new TreeNode(a[c]);
+        int rootVal = preorder[preIndex++];
+        TreeNode* root = new TreeNode(rootVal);
 
-        int m = mp[a[c++]];
+        int inIndex = mp[rootVal];
 
-        v->left = get(a, s, m - 1);
-        v->right = get(a, m + 1, f);
+        root->left = build(preorder, inStart, inIndex - 1);
+        root->right = build(preorder, inIndex + 1, inEnd);
 
-        return v;
+        return root;
     }
-    TreeNode* buildTree(vector<int>& a, vector<int>& b) {
-        for (int i = 0; i < b.size(); i++) mp[b[i]] = i;
-        
-        return get(a, 0, b.size() - 1);
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        for (int i = 0; i < inorder.size(); i++) {
+            mp[inorder[i]] = i;
+        }
+
+        return build(preorder, 0, inorder.size() - 1);
     }
 };
