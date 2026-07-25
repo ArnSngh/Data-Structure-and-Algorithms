a37dfd17d4710664111134b4e9a8b6914da089a9
Aryan Singh <aryansingh86855@gmail.com>
2026-07-25 22:44:49 +0530

    Time: 19 ms (56.1%), Space: 34.8 MB (66.89%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,25 +1,37 @@
-class Solution { // 48 ms, faster than 99.64%
+class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>> &mat) {
-        int m = mat.size(), n = mat[0].size(), INF = m + n; // The distance of cells is up to (M+N)
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (mat[r][c] == 0) continue;
-                int top = INF, left = INF;
-                if (r - 1 >= 0) top = mat[r - 1][c];
-                if (c - 1 >= 0) left = mat[r][c - 1];
-                mat[r][c] = min(top, left) + 1;
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int rows = mat.size();
+        int cols = mat[0].size();
+        vector<vector<int>> dist(rows, vector<int>(cols,-1));
+        queue<pair<int,int>> q;
+        for(int row = 0; row<rows; row++){
+            for(int col = 0; col<cols; col++){
+                if(mat[row][col] == 0){
+                    dist[row][col] = 0;
+                    q.push({row,col});
+                }
             }
         }
-        for (int r = m - 1; r >= 0; r--) {
-            for (int c = n - 1; c >= 0; c--) {
-                if (mat[r][c] == 0) continue;
-                int bottom = INF, right = INF;
-                if (r + 1 < m) bottom = mat[r + 1][c];
-                if (c + 1 < n) right = mat[r][c + 1];
-                mat[r][c] = min(mat[r][c], min(bottom, right) + 1);
+
+        int dr [] = {-1,0,1,0};
+        int dc [] = {0,1,0,-1};
+        
+        while(!q.empty()){
+            int rowa = q.front().first;
+            int cola = q.front().second;
+            q.pop();
+            for(int i =0; i<4; i++){
+                int newr = rowa+dr[i];
+                int newc = cola+dc[i];
+
+                if(newr<rows && newr>= 0 && newc>=0 && newc<cols && dist[newr][newc] == -1){
+                    dist[newr][newc] = dist[rowa][cola] + 1;
+                    q.push({newr, newc});
+                }
             }
         }
-        return mat;
+
+        return dist;
     }
 };
