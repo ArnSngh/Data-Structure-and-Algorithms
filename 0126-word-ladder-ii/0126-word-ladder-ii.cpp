7e00f1fd1595bab5d4e8db557148fabc9f2ffda4
Aryan Singh <aryansingh86855@gmail.com>
2026-07-30 03:24:14 +0530

    Time: 15 ms (46.43%), Space: 14.9 MB (8.6%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.cpp
+++ 0126-word-ladder-ii/0126-word-ladder-ii.cpp
@@ -1,66 +1,96 @@
-class Solution
-{
-public:
-    vector<vector<string>> res;
-    vector<string> te;
-    unordered_map<string, int> mp;
+class Solution {
+    vector<vector<string>> ans;
+    unordered_map<string, int> mpp;
     string b;
-    void dfs(string s)  // Step 2
+    int N;
+public:
+    // Backtracking DFS to backtrack from endWord to beginWord using the step levels in mpp
+    void dfs(string word, vector<string> seq)
     {
-        te.push_back(s);
-        if (s == b)
+        // Base case: Reached the beginWord
+        if(b == word)
         {
-            vector<string> x = te;
-            reverse(x.begin(), x.end());
-            res.push_back(x);
-            te.pop_back();
+            reverse(seq.begin(), seq.end()); // Reverse to get path from beginWord -> endWord
+            ans.push_back(seq);
+            reverse(seq.begin(), seq.end()); // Backtrack reverse to keep state clean
             return;
         }
-        int cur = mp[s];
-        for (int i = 0; i < s.size(); i++)
+        
+        // Explore all possible single-character variations
+        for(int j = 0; j < N;j++)
         {
-            char c = s[i];
-            for (char cc = 'a'; cc <= 'z'; cc++)
+            int size = mpp[word]; // Current level of the word
+            char original = word[j];
+            
+            for(char ch = 'a'; ch <= 'z';ch++)
             {
-                s[i] = cc;
-                if (mp.count(s) && mp[s] == cur - 1)
-                    dfs(s);
-            }
-            s[i] = c;
-        }
-        te.pop_back();
-        return;
-    }
-    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string> &wordList)
-    {
-        unordered_set<string> dict(wordList.begin(), wordList.end());
-        b = beginWord;
-        queue<string> q;
-        int k = beginWord.size();
-        q.push({beginWord});
-        mp[beginWord] = 0;
-        while (!q.empty())  // Step 1
-        {
-            int n = q.size();
-            while (n--)
-            {
-                string t = q.front();
-                q.pop();
-                int x = mp[t] + 1;
-                for (int i = 0; i < k; i++)
+                word[j] = ch;
+                
+                // Check if candidate word exists in our visited step map
+                if(mpp.count(word) > 0)
                 {
-                    string temp = t;
-                    for (char ch = 'a'; ch <= 'z'; ch++)
+                    // Only backtrack to nodes that are exactly 1 step closer to beginWord
+                    if(mpp[word] + 1 == size)
                     {
-                        temp[i] = ch;
-                        if (!mp.count(temp) && dict.count(temp))
-                            mp[temp] = x, q.push(temp);
+                        seq.push_back(word);
+                        dfs(word, seq);
+                        seq.pop_back(); // Backtrack step
                     }
                 }
             }
+            word[j] = original; // Restore original character
         }
-        if (mp.count(endWord))
-            dfs(endWord);
-        return res;
+    }
+
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        N = beginWord.length();
+        queue<pair<string, int>> q;
+        unordered_set<string> set(wordList.begin(), wordList.end());
+        
+        set.erase(beginWord);
+        int steps = 1;
+        q.push({beginWord, steps});
+        int length = beginWord.length();
+
+        // Step 1: BFS to find the shortest distance level from beginWord to all reachable words
+        while(!q.empty())
+        {
+            int N = q.size();
+            for(int i = 0; i < N;i++)
+            {
+                pair<string, int> check = q.front();
+                mpp[check.first] = check.second; // Store distance level
+                q.pop();
+                
+                if(check.first == endWord)
+                    break;
+                
+                string word = check.first;
+                
+                for(int j = 0; j < length;j++)
+                {
+                    char original = word[j];
+                    for(char ch = 'a'; ch <= 'z';ch++)
+                    {
+                        word[j] = ch;
+                        if(set.count(word))
+                        {
+                            set.erase(word);
+                            q.push({word, steps+1});
+                        }
+                    }
+                    word[j] = original;
+                }
+            }
+            steps++;
+        }
+
+        // Step 2: Backtrack from endWord to beginWord using DFS
+        b = beginWord;
+        vector<string> seq;
+        seq.push_back(endWord);
+        dfs(endWord, seq);
+        
+        return ans;
     }
 };
